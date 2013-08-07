107be8098dc6007dd3aa0615d4baddc3eed6c561
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2013-08-07 18:08:27 -0300

    Fixed tag check for the default deploy strategy
    Signed-off-by: Lucas Dutra Nunes <ldnunes@ossystems.com.br>

--- lib/recipes/code.rb
+++ lib/recipes/code.rb
@@ -15,7 +15,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       set(:local_repository) { application_config["local_repository"] % {:scm_local_repository_username => scm_local_repository_username} }
     end
 
-    set :check_tag, application_config["check_tag"].nil? ? true : !!application_config["repository"]
+    set :check_tag, application_config["check_tag"].nil? ? true : application_config["check_tag"]
     set(:branch) {deploy_via == :remote_cache ? get_current_commit_describe : nil}
   elsif deploy_via == :copy
     set :repository, '.'
@@ -31,28 +31,25 @@ Capistrano::Configuration.instance(:must_exist).load do
 
     desc "[internal] Fires the current git describe code to check if this deploy will succeed."
     task :check_tag do
-      if deploy_via == :remote_cache
-        # getting the branch to be used will fire the get_current_commit_describe,
-        # which stops execution if the current tag is not allowed to be used.
-        branch
-      end
+      get_current_commit_describe if deploy_via == :remote_cache
     end
   end
 
   after "deploy:update", "deploy:copy_version_describe"
+  before 'deploy', "deploy:check_tag"
   before 'deploy:cold', "deploy:check_tag"
 
   def get_current_commit_describe
     command = "git describe %{option} --tags 2> /dev/null"
-    ENV["ALLOW_ANY_COMMIT"] ? option = "" : option = "--exact-match"
-    check_tag ? option = "--always" : option = option
+    option = (ENV["ALLOW_ANY_COMMIT"] || !check_tag ? "" : "--exact-match")
     git_describe_tags = %x[#{command % {:option => option}}].strip
     if git_describe_tags.empty?
-      git_describe = %x[#{command % {:option => ""}}].strip
+      git_describe = %x[#{command % {:option => "--always"}}].strip
       puts "Could not find a tag here: " + git_describe
       puts "Please change to a valid, pushed tag, create and push a new one " +
         "or set the environment variable ALLOW_ANY_COMMIT then try again."
       exit(1)
     end
+    git_describe_tags
   end
 end
