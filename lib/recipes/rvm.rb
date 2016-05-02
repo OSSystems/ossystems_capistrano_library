d2db73c719749f104b1a62f671186f8c296384e5
Rodrigo de Almeida Pereira <contato@rodrigo-pereira.net>
2016-05-02 10:25:13 -0300

    Add task to add rvm gpg key
    Signed-off-by: Rodrigo de Almeida Pereira <contato@rodrigo-pereira.net>

--- lib/recipes/rvm.rb
+++ lib/recipes/rvm.rb
@@ -8,12 +8,18 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :rvm_install_with_sudo, true
   set(:rvm_add_to_group) { user }
   set :rvm_path, "/usr/local/rvm" # force path since rvm-capistrano tries to install in the user home dir
+  set :rvm_server_gpg_key, '409B6B1796C275462A1703113804BB82D39DC0E3'
 
   namespace :rvm do
     desc "[internal] Closes all active sessions."
     task :close_sessions do
       sessions.values.each { |session| session.close }
       sessions.clear
+    end
+
+    desc "Add RVM GPG key"
+    task :add_gpg_key do
+      sudo "gpg2 --keyserver hkp://keys.gnupg.net --recv-keys #{fetch(:rvm_server_gpg_key)}", shell: "bash"
     end
   end
 
@@ -24,6 +30,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     run "rvm#{sudo} #{rvmsudo_user} bash -c 'cd #{path} && RAILS_ENV=#{rails_env} #{env_vars} bundle exec #{command}'"
   end
 
+  before 'rvm:install_rvm', 'rvm:add_gpg_key' # set rvm gpg key
   before 'deploy:setup', 'rvm:install_rvm'
   after 'rvm:install_rvm', 'rvm:close_sessions' # restart sessions to avoid permission bugs
   before 'deploy:setup', 'rvm:install_ruby' # install Ruby and create gemset for the first time
